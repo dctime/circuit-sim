f6da66d7406128ec2a67aa7525fffd1c465a7d14
DCtime <daniel.chu916@gmail.com>
2024-11-09 22:50:52 +0800

    Feature: NMOS saturation

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <eigen3/Eigen/Dense>
 #include <functional>
 #include <iostream>
+#include <limits>
 #include <vector>
 
 class CoordinateGraph : public sf::Drawable {
@@ -106,113 +107,89 @@ public:
   }
 };
 
-double diodeCurrent(double voltage, double Is, double vt) {
-  return Is * (exp(voltage / vt) - 1);
+// only for saturation
+double calg0(double k, double vgs, double vt, double va) {
+  return ((k / 2) * pow(vgs - vt, 2)) / va;
 }
 
-double diodeGeq(double voltageDiff, double vt, double Is) {
-  return (diodeCurrent(voltageDiff, Is, vt) + Is) / vt;
+double calgm(double k, double vgs, double vds, double vt, double va) {
+  return k * (vgs - vt) * (1 + (vds - (vgs - vt)) / va);
 }
 
-double diodeIeq(double voltageDifference, double Is, double vt) {
-  double current = diodeCurrent(voltageDifference, Is, vt);
-  std::cout << "diodeIeq (current): " << current << std::endl;
-  double g = diodeGeq(voltageDifference, vt, Is);
-  std::cout << "diodeIeq (g): " << g << std::endl;
-  return current - g * voltageDifference;
+double calid(double k, double vgs, double vds, double vt, double va) {
+  return (k / 2) * pow(vgs - vt, 2) * (1 + (vds - (vgs - vt)) / va);
+}
+
+double calideq(double k, double vgs, double vds, double vt, double va) {
+  return calid(k, vgs, vds, vt, va) - calgm(k, vgs, vds, vt, va) * vgs -
+         calg0(k, vgs, vt, va) * vds;
 }
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "X-Y Coordinate Graph");
-  window.setFramerateLimit(60);
+  // nmos stats
+  double k = 0.5 * pow(10, -3);
+  double vt = 1;
+  // double va = std::numeric_limits<double>::infinity();
+  double va = 10000;
 
-  // === circuit calculation ===
-  double vt = 0.805;
-  double Is = 171 * pow(10, -9);
-
-  double R = 1000;
-
-  // init V vector
+  // G V and I matrix
   Eigen::MatrixXd v(4, 1);
-  v << 5, 4.5, 4, 1;
-  std::cout << "v: " << v.rows() << " * " << v.cols() << std::endl;
+  // initial guess
+  v << 0, 0, 0, 0;
 
-  for (int index = 0; index < 10; index++) {
-    // init G Matrix
+  for (int iteration = 0; iteration < 100; iteration++) {
     Eigen::MatrixXd g(4, 4);
-    double geq1 = diodeGeq(v(0) - v(1), vt, Is);
-    std::cout << "geq1: " << geq1 << std::endl;
-    double geq2 = diodeGeq(v(1) - v(2), vt, Is);
-    std::cout << "geq2: " << geq2 << std::endl;
-    g << geq1, -geq1, 0, 1,
-      -geq1, geq1 + geq2, -geq2, 0,
-      0, -geq2, geq2 + 1/R, 0,
-        1, 0, 0, 0;
-    std::cout << "g: " << g.rows() << " * " << g.cols() << std::endl;
-    std::cout << "g value: " << g << std::endl;
-    // init I Matrix
-    double Ieq1 = diodeIeq(v(0) - v(1), Is, vt);
-    std::cout << "Ieq1 value: " << Ieq1 << std::endl;
-    double Ieq2 = diodeIeq(v(1) - v(2), Is, vt);
-    std::cout << "Ieq2 value: " << Ieq2 << std::endl;
+    double g0;
+    g0 = calg0(k, v(0), vt, va);
+    double g00 = 0;
+    double g01 = 0;
+    double g10 = 0;
+    double g11 = g0;
+
+    g << g00, g01, 1, 0, g10, g11, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0;
+
     Eigen::MatrixXd i(4, 1);
-    i << -Ieq1, Ieq1 - Ieq2, Ieq2, -5;
-    std::cout << "i: " << i.rows() << " * " << i.cols() << std::endl;
-    // Calculate Jacobian Matrix
-    double delta = 0.000000000001;
-    Eigen::MatrixXd identityDelta = Eigen::MatrixXd::Identity(4, 4) * delta;
-    std::cout << "identityData: "  << identityDelta.rows() << " * "
-              << identityDelta.cols() << std::endl;
-    std::cout << "identityData Values:\n" << identityDelta << std::endl;
-    Eigen::MatrixXd colCopiedV = v.replicate(1, 4);
-    Eigen::MatrixXd vWithDelta = colCopiedV + identityDelta;
-    std::cout << "vWithData: " << vWithDelta.rows() << " * "
-              << vWithDelta.cols() << std::endl;
-    std::cout << "vWithData Values:\n" << vWithDelta << std::endl; 
-    Eigen::MatrixXd j = (g * vWithDelta - g * colCopiedV) / delta;
-    std::cout << "j: " << j.rows() << " * " << j.cols() << std::endl;
-    std::cout << "j values:\n" << j << std::endl;
-    // Caculate F(v)
-    Eigen::MatrixXd f = g * v - i;
-    std::cout << "f: " << f.rows() << " * " << f.cols() << std::endl;
-    // Find deltaV
-    Eigen::MatrixXd deltaV = -1 * j.inverse() * f;
-    std::cout << "deltaV: " << deltaV.rows() << " * " << deltaV.cols()
-              << std::endl;
-    // check deltaV is small enough
-    std::cout << "deltaV Values:\n" << deltaV << std::endl;
-    // apply deltaV to V
+    double issd = calgm(k, v(0), v(1), vt, va) *
+                  (v(0));
+    std::cout << "issd: " << issd << std::endl;
+    double ideq = calideq(k, v(0), v(1), vt, va);
+    std::cout << "ideq: " << ideq << std::endl;
+    i << 0, -issd - ideq, 2.5, 3;
+
+    std::cout << "g v i success" << std::endl;
+    // F matrix
+    Eigen::MatrixXd f(4, 1);
+    f = g * v - i;
+
+    std::cout << "f matrix success" << std::endl;
+    // J matrix
+    double delta = 0.00000001;
+    Eigen::MatrixXd dupV = v.replicate(1, 4);
+    Eigen::MatrixXd vWithDelta = Eigen::MatrixXd::Identity(4, 4);
+    vWithDelta = vWithDelta * delta + dupV;
+    std::cout << "vWithData value: " << vWithDelta << std::endl;
+    Eigen::MatrixXd j = (g * vWithDelta - g * dupV) / delta;
+    std::cout << "j" << std::endl;
+    std::cout << j << std::endl;
+
+    std::cout << "j matrix success" << std::endl;
+
+    // calculate deltaV
+    Eigen::MatrixXd deltaV = -1*((j.inverse()) * f);
+    std::cout << "deltaV value:" << std::endl;
+    std::cout << deltaV << std::endl;
+    // calculate new v
     v += deltaV;
-    std::cout << "Voltage Values\nV1\nV2\nV3\nI:\n" << v << std::endl;
-    std::cout << "=======================================" << std::endl;
-    std::cout << "evaluate:\n" << g * v - i << std::endl;
-    std::cout << "iteration steps: " << index+1 << std::endl;
-    // repeat
+    std::cout << "cal v success" << std::endl;
+
+    // print v
+    std::cout << "iteration: " << iteration << std::endl;
+    std::cout << "V0\nV1\nIx\nIY:" << std::endl;
+    std::cout << v << std::endl;
+    std::cout << "===============" << std::endl;
+    std::cout << "evaluation:" << std::endl;
+    std::cout << g*v-i << std::endl;
+    std::cout << "===============" << std::endl;
   }
-
-  // CoordinateGraph graph(800.0f, 600.0f, 10.0f, 1.0f);
-
-  // Example: Plot y = x^2
-  // auto quadratic = [](float x) { return x * x; };
-  // sf::VertexArray quadraticPlot = graph.plotFunction(quadratic, -4, 4);
-  // sf::VertexArray plotV1(sf::LineStrip);
-  // graph.applyVectorToPlot(valuesV1, plotV1, 0.01, sf::Color::Green);
-  // sf::VertexArray plotI1(sf::LineStrip);
-  // graph.applyVectorToPlot(valuesI1, plotI1, 0.01, sf::Color::Yellow);
-
-  // while (window.isOpen()) {
-  //   sf::Event event;
-  //   while (window.pollEvent(event)) {
-  //     if (event.type == sf::Event::Closed)
-  //       window.close();
-  //   }
-  //
-  //   window.clear(sf::Color::Black);
-  //   window.draw(graph);
-  //   window.draw(plotV1);
-  //   window.draw(plotI1);
-  //   window.display();
-  // }
-
   return 0;
 }
