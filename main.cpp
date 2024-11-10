45d1878c40c819227e660207cb407264b574de77
DCtime <daniel.chu916@gmail.com>
2024-11-10 14:38:45 +0800

    Feature: It solves circuit problems

--- main.cpp
+++ main.cpp
@@ -110,20 +110,26 @@ public:
 // change return by vgs and vds
 // only for saturation
 double calg0(double k, double vgs, double vds, double vt, double va) {
-  if (vgs - vt < 0) return 0;
-  if (vds <= vgs - vt) return k*((vgs-vt)-vds);
+  if (vgs - vt < 0)
+    return 0;
+  if (vds <= vgs - vt)
+    return k * ((vgs - vt) - vds);
   return ((k / 2) * pow(vgs - vt, 2)) / va;
 }
 
 double calgm(double k, double vgs, double vds, double vt, double va) {
-  if (vgs - vt < 0) return 0;
-  if (vds <= vgs - vt) return k*vds;
+  if (vgs - vt < 0)
+    return 0;
+  if (vds <= vgs - vt)
+    return k * vds;
   return k * (vgs - vt) * (1 + (vds - (vgs - vt)) / va);
 }
 
 double calid(double k, double vgs, double vds, double vt, double va) {
-  if (vgs - vt < 0) return 0;
-  if (vds <= vgs - vt) return k*((vgs - vt)-vds/2)*vds;
+  if (vgs - vt < 0)
+    return 0;
+  if (vds <= vgs - vt)
+    return k * ((vgs - vt) - vds / 2) * vds;
   return (k / 2) * pow(vgs - vt, 2) * (1 + (vds - (vgs - vt)) / va);
 }
 
@@ -134,45 +140,76 @@ double calideq(double k, double vgs, double vds, double vt, double va) {
 
 int main() {
   // nmos stats
-  double k = 0.4 * pow(10, -3);
+  double k = 1 * pow(10, -3);
   double vt = 1;
   // double va = std::numeric_limits<double>::infinity();
   double va = INFINITY;
 
   // G V and I matrix
-  Eigen::MatrixXd v(4, 1);
+  Eigen::MatrixXd v(7, 1);
   // initial guess
-  v << 0, 0, 0, 0;
+  v << 0, 0, 0, 0, 0, 0, 0;
 
-  for (int iteration = 0; iteration < 10; iteration++) {
-    Eigen::MatrixXd g(4, 4);
+  for (int iteration = 0; iteration < 100; iteration++) {
+    Eigen::MatrixXd g(7, 7);
     double g0;
-    g0 = calg0(k, v(0), v(1), vt, va);
-    double g00 = 0;
-    double g01 = 0;
-    double g10 = 0;
-    double g11 = g0;
+    g0 = calg0(k, v(1) - v(4), v(3) - v(4), vt, va);
+    double g1 = 1.0 / (10 * pow(10, 6));
+    double g2 = 1.0 / (10 * pow(10, 6));
+    double g3 = 1.0 / 6000.0;
+    double g4 = 1.0 / 6000.0;
 
-    g << g00, g01, 1, 0, g10, g11, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0;
+    double g00 = g1;
+    double g01 = -g1;
+    double g10 = -g1;
+    double g02 = 0;
+    double g20 = 0;
+    double g03 = 0;
+    double g30 = 0;
+    double g04 = 0;
+    double g40 = 0;
+    double g11 = g1 + g2;
+    double g12 = 0;
+    double g21 = 0;
+    double g13 = 0;
+    double g31 = 0;
+    double g14 = 0;
+    double g41 = 0;
+    double g22 = g3;
+    double g23 = -g3;
+    double g32 = -g3;
+    double g24 = 0;
+    double g42 = 0;
+    double g33 = g3 + g0;
+    double g34 = -g0;
+    double g43 = -g0;
+    double g44 = g0 + g4;
 
-    Eigen::MatrixXd i(4, 1);
-    double issd = calgm(k, v(0), v(1), vt, va) *
-                  (v(0));
+    g << g00, g01, g02, g03, g04, 1, 0,
+      g10, g11, g12, g13, g14, 0, 0,
+      g20, g21, g22, g23, g24, 0, 1,
+      g30, g31, g32, g33, g34, 0, 0,
+      g40, g41, g42, g43, g44, 0, 0,
+      1, 0, 0, 0, 0, 0, 0,
+      0, 0, 1, 0, 0, 0, 0;
+
+    Eigen::MatrixXd i(7, 1);
+    double issd = calgm(k, v(1)-v(4), v(3)-v(4), vt, va) * (v(1)-v(4));
     std::cout << "issd: " << issd << std::endl;
-    double ideq = calideq(k, v(0), v(1), vt, va);
+    double ideq = calideq(k, v(1)-v(4), v(3)-v(4), vt, va);
     std::cout << "ideq: " << ideq << std::endl;
-    i << 0, -issd - ideq, 2, 0.5;
+    i << 0, 0, 0, -issd - ideq, issd + ideq, 10, 10;
 
     std::cout << "g v i success" << std::endl;
     // F matrix
-    Eigen::MatrixXd f(4, 1);
+    Eigen::MatrixXd f(7, 1);
     f = g * v - i;
 
     std::cout << "f matrix success" << std::endl;
     // J matrix
     double delta = 0.00000001;
-    Eigen::MatrixXd dupV = v.replicate(1, 4);
-    Eigen::MatrixXd vWithDelta = Eigen::MatrixXd::Identity(4, 4);
+    Eigen::MatrixXd dupV = v.replicate(1, 7);
+    Eigen::MatrixXd vWithDelta = Eigen::MatrixXd::Identity(7, 7);
     vWithDelta = vWithDelta * delta + dupV;
     std::cout << "vWithData value: " << vWithDelta << std::endl;
     Eigen::MatrixXd j = (g * vWithDelta - g * dupV) / delta;
@@ -182,7 +219,7 @@ int main() {
     std::cout << "j matrix success" << std::endl;
 
     // calculate deltaV
-    Eigen::MatrixXd deltaV = -1*((j.inverse()) * f);
+    Eigen::MatrixXd deltaV = (-1.0/(iteration+1)) * ((j.inverse()) * f);
     std::cout << "deltaV value:" << std::endl;
     std::cout << deltaV << std::endl;
     // calculate new v
@@ -191,11 +228,11 @@ int main() {
 
     // print v
     std::cout << "iteration: " << iteration << std::endl;
-    std::cout << "V0\nV1\nIx\nIY:" << std::endl;
+    std::cout << "V0\nV1\nV2\nV3\nV4\nIx\nIY:" << std::endl;
     std::cout << v << std::endl;
     std::cout << "===============" << std::endl;
     std::cout << "evaluation:" << std::endl;
-    std::cout << g*v-i << std::endl;
+    std::cout << g * v - i << std::endl;
     std::cout << "===============" << std::endl;
   }
   return 0;
